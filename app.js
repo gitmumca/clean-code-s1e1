0244a1c5adfc8ae700ee60bc635a41f5f78730fe
gitmumca <gitmumca@mail.ru>
2022-05-28 14:53:08 +0300

    refactor: teg selector according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="item-list";
+
     label.innerText=taskString;
     label.className="task";
 
